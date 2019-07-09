06308ae340d9ca9aea945106c2dd177f26f87434
LeonAdeoye <leon.adeoye@gmail.com>
2019-07-09 23:13:28 +0800

    reducing and summarizing collector factory methods

--- src/com/leon/collect/CollectMain.java
+++ src/com/leon/collect/CollectMain.java
@@ -1,8 +1,12 @@
 package com.leon.collect;
 
-import java.util.*;
+import com.leon.streams.Dish;
 
-import static java.util.stream.Collectors.groupingBy;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.*;
 
 public class CollectMain
 {
@@ -29,6 +33,17 @@ public class CollectMain
 
     public static void main()
     {
+        List<Dish> menu = asList(
+                new Dish("pork", false, 800, Dish.Type.MEAT),
+                new Dish("beef", false, 700, Dish.Type.MEAT),
+                new Dish("chicken", false, 400, Dish.Type.MEAT),
+                new Dish("french fries", true, 530, Dish.Type.OTHER),
+                new Dish("rice", true, 350, Dish.Type.OTHER),
+                new Dish("season fruit", true, 120, Dish.Type.OTHER),
+                new Dish("pizza", true, 550, Dish.Type.OTHER),
+                new Dish("prawns", false, 300, Dish.Type.FISH),
+                new Dish("salmon", false, 450, Dish.Type.FISH));
+
         List<Transaction> transactions = Arrays.asList(new Transaction("USD"), new Transaction("GBP"), new Transaction("JPY"));
 
         Map<String, List<Transaction>> mapOfCurrencies1 = createMapOfCurrecnces(transactions);
@@ -37,5 +52,21 @@ public class CollectMain
         // groupingBy create a map whose keys are the buckets and whose values are a list of elements in those buckets.
         Map<String, List<Transaction>> mapOfCurrencies2 = transactions.stream().collect(groupingBy(Transaction::getCurrency));
         System.out.println("Second map size: " + mapOfCurrencies2.size());
+
+        System.out.println("Using collect(Collectors.counting), the dish count is " + menu.stream().collect(counting()));
+        System.out.println("Using Stream.count, the dish count is " + menu.stream().count());
+
+        //maxBy and minBy take comparators in order to determine element with max or min values.
+        Comparator<Dish> dishCaloriesComparator = Comparator.comparingInt(Dish::getCalories);
+        Optional<Dish> mostCalorieDish = menu.stream().collect(maxBy(dishCaloriesComparator));
+        menu.stream().collect(maxBy(dishCaloriesComparator)).ifPresent(d -> System.out.println("Most calorie dish: " + d));
+
+        //The Collectors class provides a specific factory method for summing: Collectors.summarizingInt.
+        // It accepts a function that maps an object into the int that has to be summarized and returns a
+        // collector that, when passed to the usual collect method, performs the requested summarization.
+        System.out.println("Calorie: " + menu.stream().collect(Collectors.summarizingInt(Dish::getCalories)));
+
+        int totalCalories = menu.stream().collect(summingInt(Dish::getCalories)); // This just get the total
+        double averageCalories = menu.stream().collect(averagingInt(Dish::getCalories)); //This get the average and return double.
     }
 }
