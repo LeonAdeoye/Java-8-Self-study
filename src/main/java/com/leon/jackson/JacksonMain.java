e7ae5aa56e8c3874401e4c9ac8cbe3fbc254190b
LeonAdeoye <leon.adeoye@gmail.com>
2020-05-18 21:13:56 +0800

    jackson object mapper code

--- src/main/java/com/leon/jackson/JacksonMain.java
+++ src/main/java/com/leon/jackson/JacksonMain.java
@@ -1,5 +1,11 @@
 package com.leon.jackson;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.List;
 
 public class JacksonMain
 {
@@ -14,6 +20,22 @@ public class JacksonMain
             String json = "{\"colour\":\"blue\", \"model\":\"toyota\"}";
             Car purpleToyota = objectMapper.readValue(json, Car.class);
             System.out.println(purpleToyota);
+
+            String jsonCarArray ="[{\"colour\":\"yellow\", \"model\":\"mercedes\"}, {\"colour\":\"green\", \"model\":\"bmw\"}]";
+            List<Car> listOfCars = objectMapper.readValue(jsonCarArray, new TypeReference<List<Car>>() {});
+            listOfCars.forEach(System.out::println);
+
+            objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+            objectMapper.configure(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES, false);
+
+            json = "{\"colour\":\"blue\", \"model\":\"nissan\", \"owner\":\"Leon\"}";
+            Car nissan = objectMapper.readValue(json, Car.class);
+            System.out.println(nissan);
+
+            DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm a z");
+            objectMapper.setDateFormat(df);
+            // Use date format with car objects that have a field with is set to epochTime
+
         }
         catch(Exception e)
         {
