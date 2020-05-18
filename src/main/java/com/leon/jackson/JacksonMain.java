ec23b9cacc2a82c5c3837fc50a8517be5529d3ee
LeonAdeoye <leon.adeoye@gmail.com>
2020-05-18 20:56:47 +0800

    added application properties for server port

--- src/main/java/com/leon/jackson/JacksonMain.java
+++ src/main/java/com/leon/jackson/JacksonMain.java
@@ -1,9 +1,23 @@
 package com.leon.jackson;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class JacksonMain
 {
     public static void main()
     {
+        try
+        {
+            ObjectMapper objectMapper = new ObjectMapper();
+            Car redBMW = new Car("red", "BMW");
+            System.out.println(objectMapper.writeValueAsString(redBMW));
 
+            String json = "{\"colour\":\"blue\", \"model\":\"toyota\"}";
+            Car purpleToyota = objectMapper.readValue(json, Car.class);
+            System.out.println(purpleToyota);
+        }
+        catch(Exception e)
+        {
+            System.err.println(e);
+        }
     }
 }
