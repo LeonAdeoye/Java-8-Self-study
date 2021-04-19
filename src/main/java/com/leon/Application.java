a2818a683bfa5878eb7603ca1a609c5b9e1ab2b1
LeonAdeoye <leon.adeoye@gmail.com>
2021-04-19 23:03:09 +0800

    bitcoin proof-of-work

--- src/main/java/com/leon/Application.java
+++ src/main/java/com/leon/Application.java
@@ -1,6 +1,7 @@
 package com.leon;
 
 import com.leon.behaviourparam.BehaviourParamMain;
+import com.leon.bitcoin.ProofOfWork;
 import com.leon.collect.CollectMain;
 import com.leon.collections.CollectionsMain;
 import com.leon.jackson.JacksonMain;
@@ -17,13 +18,14 @@ public class Application
     public static void main(String[] args)
     {
         SpringApplication.run(Application.class, args);
-        new BehaviourParamMain().main();
-        new StreamsMain().main();
-        new CollectMain().main();
-        new OptionalMain().main();
-        new CollectionsMain().main();
-        new GofMain().main();
-        new JacksonMain().main();
-        new ReactorMain().main();
+//        new BehaviourParamMain().main();
+//        new StreamsMain().main();
+//        new CollectMain().main();
+//        new OptionalMain().main();
+//        new CollectionsMain().main();
+//        new GofMain().main();
+//        new JacksonMain().main();
+//        new ReactorMain().main();
+        ProofOfWork.main();
     }
 }
