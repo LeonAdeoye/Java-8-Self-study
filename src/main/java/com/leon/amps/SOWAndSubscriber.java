bafdc968fd8f07d6e7c2455cade6f3a54dad6abf
LeonAdeoye <leon.adeoye@gmail.com>
2023-06-20 12:07:54 +0800

    SOW AND subscribe

--- src/main/java/com/leon/amps/SOWAndSubscriber.java
+++ src/main/java/com/leon/amps/SOWAndSubscriber.java
@@ -10,28 +10,24 @@ public class SOWAndSubscriber
 	public static void main() throws DisconnectedException
 	{
 		Client amps = new Client("subscriber");
-		CommandId commandId = null;
 		try
 		{
 			amps.connect("tcp://localhost:9007/amps/json?ip_protocol_prefer=ipv6");
 			amps.logon();
 
 			// Sends the command to the AMPS server.
-			MessageStream ms = amps.execute(new Command(Message.Command.SOWAndSubscribe)
-					.setTopic("orders-sow"));
+			MessageStream ms = amps.execute(new Command(Message.Command.SOWAndSubscribe).setTopic("orders-sow"));
 
 			for (Message m : ms)
 			{
 				if (m.getCommand() == Message.Command.GroupBegin)
 				{
-					System.out.println("Receiving messages from SOW " +
-							"(beginning of group).");
+					System.out.println("Receiving messages from SOW beginning of group).");
 					continue;
 				}
 				if (m.getCommand() == Message.Command.GroupEnd)
 				{
-					System.out.println("Finished receiving messages from"
-							+" SOW (end of group).");
+					System.out.println("Finished receiving messages from SOW (end of group).");
 					continue;
 				}
 				System.out.println(m.getData());
